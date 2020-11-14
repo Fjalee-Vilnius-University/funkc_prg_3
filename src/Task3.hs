803f17df99a6638e3c88e227c003150bd3bcea7b
Fjalee <cepRytis@gmail.com>
2020-11-15 00:06:58 +0200

    find step for winning

--- src/Task3.hs
+++ src/Task3.hs
@@ -282,23 +282,22 @@ delFromMap wholeMap itemDel =
 
 test = 
     let
-        board = [[(0,'b'),(1,'X'),(2,'O')],[(0,'X'),(1,'X'),(2,'b')],[(0,'b'),(1,'b'),(2,'O')]]
+        board = [[(0,'b'),(1,'X'),(2,'O')],[(0,'X'),(1,'X')],[(2,'O')]]
         board' = populateBlankVals board
-        player
-            | isXTurn board' = 'X'
-            | otherwise = 'O'
-        allBoards = genAllPossibleMoves board' board' [] player
-        allScores = calcAllBoardsScore allBoards []
     in
-        (allBoards, allScores)
+        findWinStep board'
 
-
- 
-test2 allB allS =
+findWinStep board = 
     let
-          
+        player
+            | isXTurn board = ('X', 10)
+            | otherwise = ('O', -10)
+        allPossMoves = genAllPossibleMoves board board [] (fst player)
+        allPossScores = calcAllBoardsScore allPossMoves []
     in
-        "not implemented"
+        case findIndex (== (snd player)) allPossScores of
+            Nothing -> []
+            Just i -> allPossMoves !! i
 
 isXTurn :: To -> Bool
 isXTurn ((sq1 : sq2 :sq3 : []) : (sq4 : sq5 :sq6 : [])  : (sq7 : sq8 :sq9 : [])  : []) =
